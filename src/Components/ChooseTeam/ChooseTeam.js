adac28d86b2db32f9bd389d3a322ad7b66c26e83
SShohag <shohag7m@gmail.com>
2020-11-20 12:30:10 +0600

    first commit

--- src/Components/ChooseTeam/ChooseTeam.js
+++ src/Components/ChooseTeam/ChooseTeam.js
@@ -3,9 +3,62 @@ import "./styles.scss"
 
 const ChooseTeam = () => {
     return (
-        <div>
-
+        <div className="container mt-5">
+        <h2 className="teamHeading">Choose Your Detected Team</h2>
+        <div class="card-deck mt-5">
+          <div class="card cardDiv">
+            <div class="card-body text-center cardFigure">
+              <h1 class="card-title">$199</h1>
+              <p>For Basic</p>
+              <p className="underline"></p>
+              <br/>
+              <p>Home page</p>
+              <p>4 Inner page</p>
+              <p>Asset file</p>
+              <p>Source file</p>
+              <p>Free stock photos</p>
+              <p>20 days free support</p>
+              <p>24/7 support</p>
+              <br />
+              <input className="teamButton" type="button" value="Order now" />
+            </div>
+          </div>
+          <div class="card cardDiv">
+            <div class="card-body text-center cardFigure">
+              <h1 class="card-title">$399</h1>
+              <p>For Preferred</p>
+              <center className="underline"></center>
+              <br/>
+              <p>Home page</p>
+              <p>8 Inner page</p>
+              <p>Asset file</p>
+              <p>Source file</p>
+              <p>Free stock photos</p>
+              <p>30 days free support</p>
+              <p>24/7 support</p>
+              <br />
+              <input className="teamButton" type="button" value="Order now" />
+            </div>
+          </div>
+          <div class="card cardDiv">
+            <div class="card-body text-center cardFigure">
+              <h1 class="card-title">$599</h1>
+              <p>For Elite</p>
+              <center className="underline"></center>
+              <br/>
+              <p>Home page</p>
+              <p>No Inner page</p>
+              <p>Asset file</p>
+              <p>Source file</p>
+              <p>Free stock photos</p>
+              <p>10 days free support</p>
+              <p>24/7 support</p>
+              <br />
+              <input className="teamButton" type="button" value="Order now" />
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/Components/OurAchievements/OurAchievements.js
+++ src/Components/OurAchievements/OurAchievements.js
@@ -1,11 +1,58 @@
 import React from 'react';
 import "./styles.scss"
+import happy from "../../images/happy.png";
+import marketing from "../../images/marketing.png";
+import surface from "../../images/surface.png";
+import transportation from "../../images/transportation.png";
 
 const OurAchievements = () => {
     return (
-        <div>
-
+        <div className="container mt-5">
+      <div className="row">
+        <div className="col-12 col-md-6 col-lg-4 mt-5">
+          <h2 className="header">Our Achievement</h2>
+          <p className="pText">
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letter.
+          </p>
         </div>
+
+        <div className="col-12 h-100  col-md-6 col-lg-4">
+          <div className="box1 m-3 h-75 d-flex p-4 align-items-center">
+            <img src={happy} alt="" />
+            <div className="boxText">
+              <h2 className="number">700+</h2>
+              <p className="pText">Happy Clients</p>
+            </div>
+          </div>
+          <div className="box2 m-3 h-75 d-flex p-4 align-items-center">
+            <img src={marketing} alt="" />
+            <div className="boxText">
+              <h2 className="number">140+</h2>
+              <p className="pText">Projects Completed</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-12 h-100  col-md-6 col-lg-4">
+          <div className="box2 m-3 h-75 d-flex p-4 align-items-center">
+            <img src={surface} alt="" />
+            <div className="boxText">
+              <h2 className="number">25+</h2>
+              <p className="pText">Crazy Minds</p>
+            </div>
+          </div>
+          <div className="box1 m-3 h-75 d-flex p-4 align-items-center">
+            <img src={transportation} alt="" />
+            <div className="boxText">
+              <h2 className="number">75+</h2>
+              <p className="pText">Running Projects</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     );
 };
 
